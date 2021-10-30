e4949f902205eb07ed5246be1036a608c6559e8e
Esraa <israa.eiid@gmail.com>
2021-10-30 06:20:14 +0200

    Single Responsibility Principle

---
+++ Expandable Sections/Controllers/Configurator.swift
@@ -0,0 +1,17 @@
+//
+//  Configurator.swift
+//  Expandable Sections
+//
+//  Created by Esraa on 30/10/2021.
+//
+
+import UIKit
+
+protocol Configurator {
+    func execute()
+}
+
+
+class AppDelegateConfigurator {
+    let actions: [Configurator] = [Notifications()]
+}
---
+++ Expandable Sections/Controllers/Notifications.swift
@@ -0,0 +1,16 @@
+//
+//  Notifications.swift
+//  Expandable Sections
+//
+//  Created by Esraa on 30/10/2021.
+//
+
+import UIKit
+
+class Notifications: Configurator {
+    func execute() {
+        print("notifications")
+    }
+    
+    
+}
